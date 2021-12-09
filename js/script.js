91dd0b36404b46d89b6d51b86f7f953653aa1da0
evankeith <evanmkeith@gmail.com>
2021-12-09 15:40:56 -0800

    Completed API and DOM manipulation

--- js/script.js
+++ js/script.js
@@ -20,22 +20,68 @@ const genres = [
     {id: 37, name: 'Western'}
 ]
 
+//saved movies are added to this array in order to be emailed
+let emailList = [];
+
 async function getMovieData(e) {
 	e.preventDefault();
 
     let userInputGenre = $('#genres').val();
+    //random numer to select movie list stsarting point
     let randNum = Math.floor(Math.random() * (461 - 1)) + 1;
     let randPage = 0;
-    let newMovArray = [];
     const apiKey = "5eb8160a2b66b932e3381a55d388ba1a";
-
+    //if a genre is selected it will populate this array with matching movies
+    let newMovArray = [];
+    //random movie variables
     let chosenMov;
     let moviePoster = '';
     let movieTitle = '';
     let movieRelease = '';
     let movieOverView = '';
+    
+    const setMovieVariables = (array) => {
+        chosenMov = array[Math.floor(Math.random() * array.length)];
+        moviePoster = `https://image.tmdb.org/t/p/original/${chosenMov['poster_path']}`;
+        movieTitle = `${chosenMov['title']}`;
+        movieRelease = `${chosenMov['release_date']}`;
+        movieOverView = `${chosenMov['overview']}`;
+    };
 
+    const addEmailButton = () => {
+        let listString = "";
+        for(x=0; x<emailList.length; x++){
+            listString += "Title: " + emailList[x]['title']; 
+            listString += "Overview: " + emailList[x]['overview'] + " [|] ";
+        }
+        const emailLink = $(`<a id="emailList" href="mailto:ADD EMAIL?&subject=Movie List&body=${listString}" target="_blank">Email List</a>`);
+        $('#movieList').append(emailLink);
+        console.log(emailList);
+    }
 
+    //function to generate new movie suggestion and manipulate the DOM
+    const newMovie = (poster, title, overView, release) => {
+        $('#results h2').html('');
+        $('#results h4').html('');
+        $('#results p').html('');
+        $('.saveMovie').remove();
+        $('#movTrailer').remove();
+        $('#emailList').remove();
+        $('#moviePoster').attr("src",`${poster}`);
+        $('#results h2').html(title);
+        $('#results h4').html(`Released: ${release}`);
+        $('#results p').html(overView);
+
+        const trailer = $(`<a id="movTrailer" href="https://www.youtube.com/results?search_query=${title.split("+")}+${release}+movie+trailer" target="_blank">Watch Trailer</a>`);
+        $('#results').append(trailer);
+
+        const saveBtn = $('<button class="saveMovie">Save Movie</button>');
+        $('#results').append(saveBtn); 
+
+        if($('#movieList').children().length >= 1){
+            addEmailButton();
+        };
+    };
     console.log(userInputGenre);
     //Checking what genre selected with if statement
     if(parseInt(userInputGenre) < 1){
@@ -45,32 +91,9 @@ async function getMovieData(e) {
         const response = await fetch(url);
         const data = await response.json();
 
-        chosenMov = data.results[Math.floor(Math.random() * 20)];
-        moviePoster = `https://image.tmdb.org/t/p/original/${chosenMov['poster_path']}`;
-        movieTitle = `${chosenMov['title']}`;
-        movieRelease = `${chosenMov['release_date']}`;
-        movieOverView = `${chosenMov['overview']}`;
+        setMovieVariables(data.results);
 
-        $('#results h2').html('');
-        $('#results h4').html('');
-        $('#results p').html('');
-        $('.saveMovie').remove();
-        $('#moviePoster').attr("src",`${moviePoster}`);
-        $('#results h2').html(movieTitle);
-        $('#results h4').html(`Released: ${movieRelease}`);
-        $('#results p').html(movieOverView);
-        const trailer = $(`<a href="https://www.youtube.com/results?search_query=${movieTitle.split("+")}+${movieRelease}+movie+trailer" target="_blank">Watch Trailer</a><br><br>`);
-        $('#results').append(trailer);
-
-        const saveBtn = $('<button class="saveMovie">Save Movie</button>');
-        $('#results').append(saveBtn); 
-        saveBtn.click(function(){
-            let newLiMovie = $('<li></li>');
-            newLiMovie.html(`Title: ${movieTitle}<br>Overview: ${movieOverView}<br><a href="https://www.youtube.com/results?search_query=${movieTitle.split("+")}+${movieRelease}+movie+trailer" target="_blank">Trailer</a><br><br>`)
-            $('#movieList').append(newLiMovie);
-            return
-        });
-
+        newMovie(moviePoster, movieTitle, movieOverView, movieRelease);
     } else {
         for(x=0; x<10; x++){
             //finding a random starting page then moving 5 up from that.
@@ -79,7 +102,7 @@ async function getMovieData(e) {
     
             const response = await fetch(url);
             const data = await response.json();
-
+        
             //going through each page and adding the movies that match the genre to the newMovArray 
             data.results.forEach((movie) => {
                 movie['genre_ids'].forEach((id) => {
@@ -89,29 +112,38 @@ async function getMovieData(e) {
                 });
             });
         };
+
+        //setting variables for movie 
+        setMovieVariables(newMovArray);        
         
         if(newMovArray.length === 0){
             //if no movies were found we ask the user to try again
             //first we clear the content that may have been there prior
-            $('#results h1').html('');
+            $('#results h2').html('');
+            $('#results h4').html('')
             $('#results p').html('');
             $('#results p').html("Because this is random, sometimes we come up with zero matches in the sample we've collected. Try running the same search again and I'm sure you'll find a movie! :D")
         } else {
             //if a match is found we choose a random one to show them
-            //first we clear the content that may have been there prior 
-            let chosenMov = newMovArray[Math.floor(Math.random() * newMovArray.length)];
-
-            $('#results h2').html('');
-            $('#results h4').html('');
-            $('#results p').html('');
-            $('#moviePoster').attr("src",`https://image.tmdb.org/t/p/original${chosenMov['poster_path']}`);
-            $('#results h2').html(chosenMov['title']);
-            $('#results h4').html(`Released: ${chosenMov['release_date']}`);
-            $('#results p').html(chosenMov['overview']);
+            newMovie(moviePoster, movieTitle, movieOverView, movieRelease);
         }
     };
 
-    console.log(newMovArray);    
+    //event listener to take current movie and append it to the movie list as an li
+    $('.saveMovie').click(function(){
+        let newLiMovie = $('<li></li>');
+        $('#emailList').remove();
+
+        newLiMovie.html(`<img id="savedMovie" src=${moviePoster} width= 50px><br>Title: ${movieTitle}<br>Overview: ${movieOverView}<br><a href="https://www.youtube.com/results?search_query=${movieTitle.split("+")}+${movieRelease}+movie+trailer" target="_blank">Trailer</a><br><br>`);
+
+        $('#movieList').append(newLiMovie);
+
+        emailList.push({title: movieTitle, overview: movieOverView});
+
+        if($('#movieList').children().length >= 1){
+            addEmailButton();
+        };
+    });     
 };
 
 $("#randMovForm").on("submit", getMovieData);
